0a7812e9a9b3721b3afdccf0772350ed9eb15a34
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2022-07-19 04:16:18 +0100

    add skills api

---
+++ pages/api/guidance/[roleId]/skills.js
@@ -0,0 +1,65 @@
+import RoleGuidanceService from '../../../../services/RoleGuidanceService'
+import { find, isEmpty, kebabCase, lowerCase, omitBy } from 'lodash'
+
+export default async function handler(req, res) {
+  const role = await RoleGuidanceService.getGuidance(req.query.roleId)
+
+  let data = {
+    id: role.id,
+    title: role.title,
+    description: role.description,
+    govuk_url: role.govuk_url,
+    last_updated_at: role.last_updated_at,
+    introduction: role.introduction,
+    skills: []
+  }
+
+  let _skills = {}
+
+  role.levels.forEach( level => {
+    level.skills.forEach( skill => {
+      let name = `${skill.name} as a ${role.title}`
+      let id = kebabCase(name)
+
+      if (!_skills[id]) {
+        let skillDesc = find( role.introduction.skills, { name: skill.name } )
+        skillDesc = skillDesc ? skillDesc.description : null
+
+        _skills[id] = {
+          name: name,
+          type: "skill",
+          description: skillDesc,
+          author_id: "govuk",
+          author_name: "GOVUK DDAT Framework",
+          author_logo: "https://assets.trackprogress.io/partners/govuk/branding/govuk-apple-touch-icon-180x180.png",
+          author_description: "The DDAT Capability Framework describes the job roles in the Digital, Data and Technology (DDaT) Profession and provides details of the skills needed to work at each role level.\n",
+          learn_more_url: role.govuk_url,
+          what: "",
+          why: "",
+          resources: [
+            {
+              label: `${role.title} (DDAT Framework)`,
+              href: role.govuk_url,
+            }
+          ],
+          milestones: {awareness: {}, working: {}, practitioner: {}, expert: {}},
+        }
+      }
+
+      _skills[id].milestones[ lowerCase(skill.level) ] = {
+        name: skill.level,
+        description: skill.level_requirements.join(' '),
+        checklist: skill.requirements.map( requirement => ({
+          summary: requirement,
+        }) )
+      }
+    })
+  })
+
+  Object.values(_skills).forEach( skill => {
+    skill.milestones = Object.values(omitBy(skill.milestones, (v) => isEmpty(v)))
+    data.skills.push(skill)
+  } )
+
+  res.status(200).json( data )
+}
