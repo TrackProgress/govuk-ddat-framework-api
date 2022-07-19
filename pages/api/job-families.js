ba4ca7ac71079349f507c42eed10791a285d8d86
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2022-07-19 02:57:04 +0100

    change url to env

--- pages/api/job-families.js
+++ pages/api/job-families.js
@@ -1,9 +1,5 @@
 import JobFamilyService from '../../services/JobFamilyService'
 
 export default async function handler(req, res) {
-  const proto =
-    req.headers["x-forwarded-proto"] || req.connection.encrypted
-      ? "https"
-      : "http";
-  res.status(200).json( await JobFamilyService.getJobFamilyList(`${proto}://${req.headers.host}`) )
+  res.status(200).json( await JobFamilyService.getJobFamilyList(process.env.HOST_URL) )
 }
